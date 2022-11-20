867745cf88bf03ebf1e6953609cc750612f54384
Rodgers Ogada <ogadarodgers@gmail.com>
2022-11-20 19:03:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+// no longer has DOM node 'main#main'
+main.remove();
+
+// has a 'newHeader' variable that points to an <h1> node
+const newHeader = document.createElement("h1");
+
+// the 'newHeader' variable that points to the <h1> node has an id of 'victory'
+newHeader.setAttribute('id', 'victory');
+
+// the 'newHeader' variable that points to the <h1> node with an id of 'victory' has the text "YOUR-NAME is the champion" 
+// (with your name in place of YOUR-NAME) inside it
+newHeader.textContent = 'Ogada is the champion';
